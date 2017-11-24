cd7733c518497821736a982dce5516866ecc0d21
Domenico Rizzo <dev@idsign.it>
2017-11-24 10:29:41 +0100

    Added Environment Middleware

---
+++ Contracts/HttpCodes.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Willypuzzle\Helpers\Contracts;
+
+interface HttpCodes {
+    const FORBIDDEN = 403;
+    const NOT_FOUND = 404;
+    const UNAUTHORIZED = 401;
+}
---
+++ Middleware/Environment.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Willypuzzle\Helpers\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Willypuzzle\Helpers\Facades\General\Environment as EnvironmentFacade;
+use Willypuzzle\Helpers\Contracts\HttpCodes;
+
+class Environment
+{
+
+    protected $redirect = null;
+    protected $view = null;
+
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $environment
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $environment = null)
+    {
+        if($environment === null){
+            $environment = [true, false, false];
+        }else{
+            $environment = $this->parseEnvironment($environment);
+        }
+
+        if(!EnvironmentFacade::is(...$environment)){
+            if($request->ajax()){
+                return response()->json(['error' => 'Environment doesn\'t allow this action'], HttpCodes::FORBIDDEN);
+            }else{
+                if($this->view){
+                    return view($this->view);
+                }else if ($this->redirect){
+                    return redirect($this->redirect);
+                }else{
+                    return 'Environment doesn\'t allow this action';
+                }
+            }
+        }
+
+
+        return $next($request);
+    }
+
+    private function parseEnvironment($environment)
+    {
+        $environment = explode(',', $environment);
+
+        $result = [
+            false,
+            false,
+            false
+        ];
+
+        foreach ($environment as $env){
+            switch ($env){
+                case 'develop':
+                    $result[0] = true;
+                    break;
+                case 'testing':
+                    $result[1] = true;
+                    break;
+                case 'production':
+                    $result[2] = true;
+                    break;
+            }
+        }
+
+        return $result;
+    }
+}
