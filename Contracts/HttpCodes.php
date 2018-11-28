2d5f02155386cde1c03d3b41e50c646e2147b00e
Domenico Rizzo <dev@idsign.it>
2018-11-28 11:51:13 +0100

    Added CREATED constant to http codes

--- Contracts/HttpCodes.php
+++ Contracts/HttpCodes.php
@@ -7,6 +7,7 @@ interface HttpCodes {
     const FORBIDDEN = 403;
     const NOT_FOUND = 404;
     const OK = 200;
+    const CREATED = 201;
     const UNAUTHORIZED = 401;
     const INTERNAL_SERVER_ERROR = 500;
     const UNPROCESSABLE_ENTITY = 422;
