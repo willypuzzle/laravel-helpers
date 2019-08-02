4a3cac45c77bca48a5213b0d729400f411d35a1a
Domenico Rizzo <dev@idsign.it>
2019-08-02 17:20:24 +0200

    Added methods in array facade

--- Facades/General/Arrays.php
+++ Facades/General/Arrays.php
@@ -2,7 +2,15 @@
 namespace Willypuzzle\Helpers\Facades\General;
 
 use Illuminate\Support\Facades\Facade;
-
+/**
+ * @method boolean arrayEqual(array $a, array $b)
+ * @method boolean isSubset(array $container, array $contained)
+ * @method object toObj(array $array)
+ * @method array pick(array $array, $fields = [])
+ * @method mixed get($array, $path, $default = null)
+ * @method array set($array, $path, $value)
+ * @method boolean isAssociative($arr)
+ */
 class Arrays extends Facade {
 
     /**
