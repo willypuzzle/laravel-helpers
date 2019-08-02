7d6d60b1e9c2756ad24d97921ee6a1720ce00ea1
Domenico Rizzo <dev@idsign.it>
2019-08-02 17:30:10 +0200

    corrected doc

--- Facades/General/Arrays.php
+++ Facades/General/Arrays.php
@@ -3,13 +3,13 @@ namespace Willypuzzle\Helpers\Facades\General;
 
 use Illuminate\Support\Facades\Facade;
 /**
- * @method boolean arrayEqual(array $a, array $b)
- * @method boolean isSubset(array $container, array $contained)
- * @method object toObj(array $array)
- * @method array pick(array $array, $fields = [])
- * @method mixed get($array, $path, $default = null)
- * @method array set($array, $path, $value)
- * @method boolean isAssociative($arr)
+ * @method static boolean arrayEqual(array $a, array $b)
+ * @method static boolean isSubset(array $container, array $contained)
+ * @method static object toObj(array $array)
+ * @method static array pick(array $array, $fields = [])
+ * @method static mixed get($array, $path, $default = null)
+ * @method static array set($array, $path, $value)
+ * @method static boolean isAssociative($arr)
  */
 class Arrays extends Facade {
 
