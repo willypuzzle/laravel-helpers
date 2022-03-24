bcb49f1d2b0f01c95f5e8fd5c2c9f974d8f0a59b
Domenico <d.rizzo@idsign.it>
2022-03-24 10:23:28 +0100

    added doc to Environment

--- Facades/General/Environment.php
+++ Facades/General/Environment.php
@@ -3,6 +3,12 @@ namespace Willypuzzle\Helpers\Facades\General;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static bool isProduction()
+ * @method static bool isTesting()
+ * @method static bool isDevelop()
+ * @method static bool is(bool $develop = true, bool $testing = false, bool $production = false)
+ */
 class Environment extends Facade {
 
     /**
